d3944be8afd39474741d5200b8025814c68f9dba
Odair Jacinto da Silva <odairjacintosilva@gmail.com>
2019-04-17 15:36:04 -0300

    Update maior3.c

--- maior3.c
+++ maior3.c
@@ -1,13 +1,20 @@
+/*
+Este programa recebe 3 valores nas variáveis a,b e c. 
+Compara os 3 valore sentre si e apresenta, como resultado, o maior
+entre eles.
+*/
 #include <stdio.h>
 main()
 {
     int a,b,c;
+    //Entrada de dados
     printf("a=");
     scanf("%d",&a);
     printf("b=");
     scanf("%d",&b);    
     printf("c=");
     scanf("%d",&c);
+    //Comparação e apresentação do resultado
     if(a>b)
         if(a>c)
             printf("Maior %d",a);
